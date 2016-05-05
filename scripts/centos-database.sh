e7336632198d05c27defce25900781d5281c1ab6
aliimirza <alii.mirza@yahoo.com>
2016-05-05 08:38:09 -0400

    open inbound traffic to 3306, set mysqld to start

--- scripts/centos-database.sh
+++ scripts/centos-database.sh
@@ -7,3 +7,9 @@ yum install -y mysql mysql-server mysql-devel
 chkconfig --add mysqld
 chkconfig mysqld on
 
+# Allow inbound traffic on port 3306
+iptables -I INPUT 3 -p tcp --dport 3306 -j ACCEPT
+/etc/init.d/iptables save
+
+# Start mysqld
+service mysqld start
